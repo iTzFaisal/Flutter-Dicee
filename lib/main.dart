4be04e4e8fe36cb0dcf83791d951e09f06d000a0
Faisal Alkheraiji <FaisalAlkheraiji@gmail.com>
2020-04-20 12:04:23 +0300

    1. Update README file 2. Build the UI with logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,6 +9,7 @@ void main() {
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
+          centerTitle: true,
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,42 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png')),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
